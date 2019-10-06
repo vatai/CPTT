db22100b6eb6373e7519c08a2717c3e0120279e1
Emil VATAI <emil.vatai@gmail.com>
2019-10-06 19:04:14 +0900

    Add some code to Lexer.java

---
+++ ch01/Lexer.java
@@ -0,0 +1,10 @@
+package lexer;
+
+import java.io.*;
+import java.util.*;
+
+public class Lexer {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!\n");
+    }
+}
